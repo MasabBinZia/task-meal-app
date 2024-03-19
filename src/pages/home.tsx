71cf40a069d96363f2e5251f87074a7aded24677
MasabBinZia <masabmbz5@gmail.com>
2024-03-19 02:58:20 -0700

    xyz

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,15 +6,15 @@ function HomePage() {
     <main className="flex flex-col justify-center items-center py-20 w-full h-full">
       <h1 className="text-6xl text-primary font-bold">Home Page</h1>
       <div className="py-12 grid grid-cols-3 justify-center items-center gap-6">
-        <Link to={"/menu"}>
+        <a href={"/menu"}>
           <Button className="w-full">Menu</Button>
-        </Link>
-        <Link to={"/favorites"}>
+        </a>
+        <a href={"/favorites"}>
           <Button className="w-full">Favorites</Button>
-        </Link>
-        <Link to={"/random-meal"}>
+        </a>
+        <a href={"/random-meal"}>
           <Button className="w-full">Random Meal</Button>
-        </Link>
+        </a>
       </div>
     </main>
   );
